699d6586c055e25c9d114a8bd42b523a4e542005
Michael Kirkpatrick <michael.kirkpatrick@mymail.champlain.edu>
2023-09-11 11:15:12 -0400

    Tweaks

--- ThirdPersonV2/Source/ThirdPersonV2/Public/EnemyController.h
+++ ThirdPersonV2/Source/ThirdPersonV2/Public/EnemyController.h
@@ -9,21 +9,33 @@
 #include "AIController.h"
 #include "EnemyController.generated.h"
 
+struct ForceConstants
+{
+
+public:
+	const float _wanderingConstant = 1000;
+	const float _separationConstant = 1000;
+};
+
 UCLASS()
 class THIRDPERSONV2_API AEnemyController : public AAIController
 {
-public:
-
 	GENERATED_BODY()
 
+public:
 	UFUNCTION(BlueprintCallable, Category = "Enemy|Utility|")
 		void InitController();
 
 	UFUNCTION(BlueprintCallable, Category = "Enemy|Movement|")
-	void AddForceDirection(FVector directionToBeAdded);
+		void AddForceDirection(FVector directionToBeAdded);
 
 	UFUNCTION(BlueprintCallable, Category = "Enemy|Movement|")
-	void Flocking(const TArray<AActor*>& neighbours);
+		void Flocking(const TArray<AActor*>& neighbours);
+
+	//Flocking Calculations
+	void CalculateSeperationDirection(const TArray<AActor*>& neighbours);
+	void CalculateCohesionDirection(const TArray<AActor*>& neighbours);
+	void CalculateAlignmentDirection(const TArray<AActor*>& neighbours);
 
 	void SetCurrentForceDirection(FVector dir) { _currentForceDirection = dir; }
 	FVector GetCurrentForceDirection() const { return _currentForceDirection; }
