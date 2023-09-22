a95ae5f8a5c828861556ee236242fb28ef4bf109
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2023-09-22 00:10:09 -0400

    Animations & Movement
    Clients can't see rotation, or jump off platform, camera gets stuck on other players

---
+++ Project_Steel/Source/Project_Steel/Player/PlayerAnimInstance.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerAnimInstance.h"
+
+#include "PlayerCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	PlayerCharacter = Cast<APlayerCharacter>(TryGetPawnOwner());
+}
+
+void UPlayerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if(PlayerCharacter == nullptr)
+	{
+		PlayerCharacter = Cast<APlayerCharacter>(TryGetPawnOwner());
+	}
+	if(PlayerCharacter == nullptr) return;
+
+	FVector Velocity = PlayerCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	bIsInAir = PlayerCharacter->GetCharacterMovement()->IsFalling();
+	bIsAccelerating = PlayerCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+
+}
---
+++ Project_Steel/Source/Project_Steel/Player/PlayerAnimInstance.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_STEEL_API UPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	class APlayerCharacter* PlayerCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+};
