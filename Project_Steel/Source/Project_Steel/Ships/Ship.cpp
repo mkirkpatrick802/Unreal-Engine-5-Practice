13759ca3288b01db9860473ff2c3c99a6be8ee12
Michael Kirkpatrick <michael.kirkpatrick@mymail.champlain.edu>
2023-10-19 10:06:14 -0400

    Ship Class Replicating
    TODO: Implement Cockpit  & Turret, Add Ship Controls
    TODO: Make Preview Get Blocked by Players

--- Project_Steel/Source/Project_Steel/Ships/Ship.cpp
+++ Project_Steel/Source/Project_Steel/Ships/Ship.cpp
@@ -8,11 +8,18 @@ AShip::AShip()
 	USceneComponent* Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
 	Root->SetupAttachment(Root);
 	SetRootComponent(Root);
+
+	bReplicates = true;
 }
 
 void AShip::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AShip::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 }
 
 void AShip::BeginPlay()
